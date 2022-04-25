e76f86f36df730c48e5381913cfcb53a521d123c
Anjan Roy <hello@itzmeanjan.in>
2022-04-25 11:16:28 +0530

    tinyjambu-256 state update function
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/permute.hpp
+++ include/permute.hpp
@@ -136,3 +136,63 @@ state_update(uint32_t* const __restrict state,    // 128 -bit permutation state
 }
 
 }
+
+// TinyJambu-256 Authenticated Encryption with Associated Data Implementation
+namespace tinyjambu_256 {
+
+// TinyJambu-256 `StateUpdate` function, computing 128 feedback bits during each
+// iteration ( see inside body of for loop )
+//
+// Note, this function will update state of 128 -bit Non-Linear Feedback Shift
+// Register `rounds` -many time; so ensure that ( a compile-time check is
+// in-place )
+//
+// assert rounds % 128 == 0
+//
+// See section 3.2.3 in TinyJambu specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/tinyjambu-spec-final.pdf
+template<const size_t rounds>
+static inline void
+state_update(uint32_t* const __restrict state,    // 128 -bit permutation state
+             const uint32_t* const __restrict key // 256 -bit secret key
+             ) requires(check_rounds(rounds))
+{
+  const size_t itr_cnt = rounds >> 7;
+
+  for (size_t i = 0, j = 0; i < itr_cnt; i++, j += 4) {
+    {
+      const uint32_t s47 = (state[2] << 17) | (state[1] >> 15);
+      const uint32_t s70 = (state[3] << 26) | (state[2] >> 6);
+      const uint32_t s85 = (state[3] << 11) | (state[2] >> 21);
+      const uint32_t s91 = (state[3] << 5) | (state[2] >> 27);
+
+      state[0] = state[0] ^ s47 ^ (~(s70 & s85)) ^ s91 ^ key[(j + 0ul) & 7ul];
+    }
+    {
+      const uint32_t s47 = (state[3] << 17) | (state[2] >> 15);
+      const uint32_t s70 = (state[0] << 26) | (state[3] >> 6);
+      const uint32_t s85 = (state[0] << 11) | (state[3] >> 21);
+      const uint32_t s91 = (state[0] << 5) | (state[3] >> 27);
+
+      state[1] = state[1] ^ s47 ^ (~(s70 & s85)) ^ s91 ^ key[(j + 1ul) & 7ul];
+    }
+    {
+      const uint32_t s47 = (state[0] << 17) | (state[3] >> 15);
+      const uint32_t s70 = (state[1] << 26) | (state[0] >> 6);
+      const uint32_t s85 = (state[1] << 11) | (state[0] >> 21);
+      const uint32_t s91 = (state[1] << 5) | (state[0] >> 27);
+
+      state[2] = state[2] ^ s47 ^ (~(s70 & s85)) ^ s91 ^ key[(j + 2ul) & 7ul];
+    }
+    {
+      const uint32_t s47 = (state[1] << 17) | (state[0] >> 15);
+      const uint32_t s70 = (state[2] << 26) | (state[1] >> 6);
+      const uint32_t s85 = (state[2] << 11) | (state[1] >> 21);
+      const uint32_t s91 = (state[2] << 5) | (state[1] >> 27);
+
+      state[3] = state[3] ^ s47 ^ (~(s70 & s85)) ^ s91 ^ key[(j + 3ul) & 7ul];
+    }
+  }
+}
+
+}
