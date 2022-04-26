4b6ff2dc7670feb83a7bf41c0c57d89ca2c2f5c9
Anjan Roy <hello@itzmeanjan.in>
2022-04-26 11:54:49 +0530

    drive testing on tinyjambu-{192, 256} aead
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,6 @@
 #include "test_tinyjambu_128.hpp"
+#include "test_tinyjambu_192.hpp"
+#include "test_tinyjambu_256.hpp"
 #include <iostream>
 
 int
@@ -7,7 +9,7 @@ main()
   constexpr size_t MIN_DT_LEN = 0ul;
   constexpr size_t MAX_DT_LEN = 32ul;
   constexpr size_t MIN_CT_LEN = 0ul;
-  constexpr size_t MAX_CT_LEN = 32ul;
+  constexpr size_t MAX_CT_LEN = 64ul;
 
   for (size_t i = MIN_CT_LEN; i < MAX_CT_LEN; i++) {
     for (size_t j = MIN_DT_LEN; j < MAX_DT_LEN; j++) {
@@ -16,6 +18,22 @@ main()
   }
 
   std::cout << "[test] passed TinyJambu-128 AEAD" << std::endl;
+
+  for (size_t i = MIN_CT_LEN; i < MAX_CT_LEN; i++) {
+    for (size_t j = MIN_DT_LEN; j < MAX_DT_LEN; j++) {
+      test_tinyjambu::key_192(j, i);
+    }
+  }
+
+  std::cout << "[test] passed TinyJambu-192 AEAD" << std::endl;
+
+  for (size_t i = MIN_CT_LEN; i < MAX_CT_LEN; i++) {
+    for (size_t j = MIN_DT_LEN; j < MAX_DT_LEN; j++) {
+      test_tinyjambu::key_256(j, i);
+    }
+  }
+
+  std::cout << "[test] passed TinyJambu-256 AEAD" << std::endl;
 
   return EXIT_SUCCESS;
 }
