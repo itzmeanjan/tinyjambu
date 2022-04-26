5e5645420ca562aa52cbef300e7a51fc01a34060
Anjan Roy <hello@itzmeanjan.in>
2022-04-26 11:47:42 +0530

    tinyjambu-192 encrypt/ decrypt implementation
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/tinyjambu_192.hpp
+++ include/tinyjambu_192.hpp
@@ -1,16 +1,16 @@
 #pragma once
 #include "tinyjambu.hpp"
 
-// TinyJambu-128 Authenticated Encryption with Associated Data Implementation
-namespace tinyjambu_128 {
+// TinyJambu-192 Authenticated Encryption with Associated Data Implementation
+namespace tinyjambu_192 {
 
-// TinyJambu-128 Authenticated Encryption, which takes 128 -bit secret key, 96
+// TinyJambu-192 Authenticated Encryption, which takes 192 -bit secret key, 96
 // -bit public message nonce, N -bytes of associated data ( which is never
 // encrypted ) & M -bytes of plain text ( which is encrypted ), producing M
 // -bytes of cipher text & 64 -bit of authentication tag --- providing
 // confidentiality, integrity & authentication
 static inline void
-encrypt(const uint8_t* const __restrict key,   // 128 -bit secret key
+encrypt(const uint8_t* const __restrict key,   // 192 -bit secret key
         const uint8_t* const __restrict nonce, // 96 -bit public message nonce
         const uint8_t* const __restrict data,  // associated data
         const size_t data_len,                 // associated data byte length
@@ -23,23 +23,23 @@ encrypt(const uint8_t* const __restrict key,   // 128 -bit secret key
   using namespace tinyjambu;
 
   // note permutation state must be zero initialized !
-  uint32_t state[4] = { 0u };
+  uint32_t state[6] = { 0u };
   uint32_t key_[4] = { 0u };
 
 #if defined __clang__
-#pragma unroll 4
+#pragma unroll 6
 #endif
-  for (size_t i = 0; i < 4; i++) {
+  for (size_t i = 0; i < 6; i++) {
     key_[i] = from_le_bytes(key + (i << 2));
   }
 
-  initialize<key_128>(state, key_, nonce);
-  process_associated_data<key_128>(state, key_, data, data_len);
-  process_plain_text<key_128>(state, key_, text, cipher, ct_len);
-  finalize<key_128>(state, key_, tag);
+  initialize<key_192>(state, key_, nonce);
+  process_associated_data<key_192>(state, key_, data, data_len);
+  process_plain_text<key_192>(state, key_, text, cipher, ct_len);
+  finalize<key_192>(state, key_, tag);
 }
 
-// TinyJambu-128 Verified Decryption, which takes 128 -bit secret key, 96
+// TinyJambu-192 Verified Decryption, which takes 192 -bit secret key, 96
 // -bit public message nonce, 64 -bit authentication tag, N -bytes of associated
 // data ( which was never encrypted ) & M -bytes of cipher text, producing M
 // -bytes of plain text & boolean verification flag denoting authentication
@@ -48,7 +48,7 @@ encrypt(const uint8_t* const __restrict key,   // 128 -bit secret key
 // Note, if returned boolean verification status is not truth value, don't
 // consume decrypted bytes !
 static inline bool
-decrypt(const uint8_t* const __restrict key,    // 128 -bit secret key
+decrypt(const uint8_t* const __restrict key,    // 192 -bit secret key
         const uint8_t* const __restrict nonce,  // 96 -bit public message nonce
         const uint8_t* const __restrict tag,    // 64 -bit authentication tag
         const uint8_t* const __restrict data,   // associated data
@@ -61,21 +61,21 @@ decrypt(const uint8_t* const __restrict key,    // 128 -bit secret key
   using namespace tinyjambu;
 
   // note permutation state must be zero initialized !
-  uint32_t state[4] = { 0u };
+  uint32_t state[6] = { 0u };
   uint32_t key_[4] = { 0u };
   uint8_t tag_[8] = { 0u };
 
 #if defined __clang__
-#pragma unroll 4
+#pragma unroll 6
 #endif
-  for (size_t i = 0; i < 4; i++) {
+  for (size_t i = 0; i < 6; i++) {
     key_[i] = from_le_bytes(key + (i << 2));
   }
 
-  initialize<key_128>(state, key_, nonce);
-  process_associated_data<key_128>(state, key_, data, data_len);
-  process_cipher_text<key_128>(state, key_, cipher, text, ct_len);
-  finalize<key_128>(state, key_, tag_);
+  initialize<key_192>(state, key_, nonce);
+  process_associated_data<key_192>(state, key_, data, data_len);
+  process_cipher_text<key_192>(state, key_, cipher, text, ct_len);
+  finalize<key_192>(state, key_, tag_);
 
   bool flag = false;
   for (size_t i = 0; i < 8; i++) {
